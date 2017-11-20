acefb9699061aae1eaad87a58276ce4949e0ed67
Shrivu Shankar <shrivu1122@gmail.com>
2017-11-19 19:54:13 -0600

    Changed Faculty Endpoint

--- cfisdapi/faculty.py
+++ cfisdapi/faculty.py
@@ -2,7 +2,7 @@ from cfisdapi import app
 
 FACULTY_FILE = 'faculty.json'
 
-@app.route("/faculty")
+@app.route("/api/faculty/list")
 def get_faculty():
     """Returns (static) json of school faculty."""
     with open(FACULTY_FILE, 'r') as ff:
