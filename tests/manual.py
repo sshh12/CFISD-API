6f3b04fe1ec59631426900755daf70533e8dc48a
Shrivu Shankar <shrivu1122@gmail.com>
2018-01-12 17:01:52 -0600

    Fixed Test

--- tests/manual.py
+++ tests/manual.py
@@ -1,7 +1,7 @@
 import getpass
 import pprint
 
-from cfisdapi.grades import HomeAccessCenter
+from cfisdapi.grades import HomeAccessCenterUser
 
 def main(username, password):
     """
@@ -10,12 +10,13 @@ def main(username, password):
     Manually interacts with the api with given creds and prints
     the returned recursive dict/json.
     """
-    hac = HomeAccessCenter(username)
+    hac = HomeAccessCenterUser(username)
     hac.login(password)
 
     pp = pprint.PrettyPrinter(indent=3)
     pp.pprint(hac.get_classwork())
     pp.pprint(hac.get_reportcard())
+    pp.pprint(hac.get_transcript())
 
 
 if __name__ == "__main__":
