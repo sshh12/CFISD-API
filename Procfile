bc68f04eed4cb6781a934401946a75f53c1bba69
Shrivu Shankar <shrivu1122@gmail.com>
2018-12-17 12:26:50 -0600

    Firebase Broke

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn manage:app --timeout 40 --max-requests 2048 --log-file -
+web: gunicorn manage:app --timeout 40 --max-requests 10000 --log-file -
--- cfisdapi/database.py
+++ cfisdapi/database.py
@@ -24,53 +24,65 @@ db_users = db.collection(u'users')
 
 def set_grade(user, subject, name, grade, gradetype):
     """Sets a users grade in the db"""
-
-    db_users.document(user).collection(u'grades').document(fb_encode(name)).set({
-        u'name': name,
-        u'subject': subject,
-        u'grade': grade,
-        u'gradetype': gradetype,
-        u'lastupdated': datetime.now()
-    })
+    try:
+        db_users.document(user).collection(u'grades').document(fb_encode(name)).set({
+            u'name': name,
+            u'subject': subject,
+            u'grade': grade,
+            u'gradetype': gradetype,
+            u'lastupdated': datetime.now()
+        })
+    except Exception as e:
+        print(e)
 
 def add_user(user, demo):
-
-    db_users.document(user).collection(u'profile').document(u'userdata').set({
-        u'name': demo['name'],
-        u'school': demo['school'],
-        u'language': demo['language'],
-        u'gender': demo['gender'],
-        u'gradelevel': demo['gradelevel'],
-        u'lastupdated': datetime.now()
-    })
+    try:
+        db_users.document(user).collection(u'profile').document(u'userdata').set({
+            u'name': demo['name'],
+            u'school': demo['school'],
+            u'language': demo['language'],
+            u'gender': demo['gender'],
+            u'gradelevel': demo['gradelevel'],
+            u'lastupdated': datetime.now()
+        })
+    except Exception as e
+        print(e)
 
 def add_rank(user, transcript):
     """Adds a users class rank to db"""
-
-    db_users.document(user).collection(u'transcript').document(u'gpa').set({
-        u'gpa': transcript['gpa']['value'],
-        u'rank': transcript['gpa']['rank'],
-        u'classsize': transcript['gpa']['class_size'],
-        u'lastupdated': datetime.now()
-    })
+    try:
+        db_users.document(user).collection(u'transcript').document(u'gpa').set({
+            u'gpa': transcript['gpa']['value'],
+            u'rank': transcript['gpa']['rank'],
+            u'classsize': transcript['gpa']['class_size'],
+            u'lastupdated': datetime.now()
+        })
+    except Exception as e
+        print(e)
 
 def add_news(school, organization, eventdate, text, link, picture, type_):
     """Adds new article to db"""
-    db_news.document(fb_encode(school)).collection(u'articles').document(fb_encode(text)).set({
-        u'school': school,
-        u'text': text,
-        u'organization': organization,
-        u'link': link,
-        u'picture': picture,
-        u'articletype': type_,
-        u'date': eventdate,
-        u'lastupdated': datetime.now()
-    })
+    try:
+        db_news.document(fb_encode(school)).collection(u'articles').document(fb_encode(text)).set({
+            u'school': school,
+            u'text': text,
+            u'organization': organization,
+            u'link': link,
+            u'picture': picture,
+            u'articletype': type_,
+            u'date': eventdate,
+            u'lastupdated': datetime.now()
+        })
+    except Exception as e
+        print(e)
 
 def get_news(school):
     """Gets all news from db"""
+    try:
+        all_news = db_news.document(fb_encode(school)).collection(u'articles').get()
 
-    all_news = db_news.document(fb_encode(school)).collection(u'articles').get()
-
-    for article in all_news:
-        yield article.to_dict()
+        for article in all_news:
+            yield article.to_dict()
+    except Exception as e
+        print(e)
+        return []
